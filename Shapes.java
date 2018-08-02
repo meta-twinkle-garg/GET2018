65bb58916f7ab38eaad6da97f93f3f9a2b3e2def
meta-twinkle-garg <41181674+meta-twinkle-garg@users.noreply.github.com>
2018-08-02 14:19:16 +0530

    Update Shapes.java

--- Shapes.java
+++ Shapes.java
@@ -1,7 +1,17 @@
 package GraphicsLibrary;
 
 import java.util.Date;
-
+/***
+* Interface shapes contains various methods to be implemented on shapes:
+* 1. return area
+* 2. return perimeter
+* 3. check if point is enclosed
+* 4. return origin
+* 5. return id
+* 6. get type of shape
+* 7. set timestamp
+* 8. return time stamp
+*/
 public interface Shapes {
     public enum ShapeType {
         CIRCLE,RECTANGLE,SQUARE,TRIANGLE,REGULAR_POLYGON
