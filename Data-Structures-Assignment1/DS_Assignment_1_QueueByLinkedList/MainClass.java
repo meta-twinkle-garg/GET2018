a15e031c17351a0642e496ccc8509d9181239143
meta-twinkle-garg <41181674+meta-twinkle-garg@users.noreply.github.com>
2018-08-03 11:29:20 +0530

    Update MainClass.java

--- Data-Structures-Assignment1/DS_Assignment_1_QueueByLinkedList/MainClass.java
+++ Data-Structures-Assignment1/DS_Assignment_1_QueueByLinkedList/MainClass.java
@@ -1,4 +1,16 @@
 
 public class MainClass {
 
+    public static void main(String args[]){
+        QueueByLinkedList<Integer> queue=new QueueByLinkedList<Integer>();
+        queue.enqueue(5);
+        queue.enqueue(10);
+        queue.display();
+        System.out.println("deleted element is: "+queue.dequeue());
+        queue.display();
+        System.out.println(queue.isEmpty());
+        queue.dequeue();
+        System.out.println(queue.isEmpty());
+        
+    }
 }
