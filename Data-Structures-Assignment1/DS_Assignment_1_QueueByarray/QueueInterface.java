74e82ede7ce6d06101c7bb7c516bf713254fc339
meta-twinkle-garg <41181674+meta-twinkle-garg@users.noreply.github.com>
2018-08-03 11:05:46 +0530

    Update QueueInterface.java

--- Data-Structures-Assignment1/DS_Assignment_1_QueueByarray/QueueInterface.java
+++ Data-Structures-Assignment1/DS_Assignment_1_QueueByarray/QueueInterface.java
@@ -1,4 +1,20 @@
 
-public class QueueInterface {
 
+/***
+ * This interface is for queue implementation. It contains methods to be performed on queue.
+ * 1. add an element to queue
+ * 2. delete an element from queue 
+ * 3. check if queue is full
+ * 4. check if queue is empty
+ * @author Twinkle-Garg
+ *
+ * @param <GenericType> specified by user
+ */
+public interface QueueInterface<GenericType> {
+    public void enqueue(GenericType value);
+    public GenericType dequeue();
+    public void display();
+    public boolean isEmpty();
+    public boolean isFull();
 }
+
