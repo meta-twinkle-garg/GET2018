e3c6469fca2a7b8b494fe97b0983edcc181c4414
meta-twinkle-garg <41181674+meta-twinkle-garg@users.noreply.github.com>
2018-08-21 13:05:25 +0530

    Update assignment4.sql

--- DBMS-Session4/assignment4.sql
+++ DBMS-Session4/assignment4.sql
@@ -1,4 +1,11 @@
 use storefront;
+DROP FUNCTION IF EXISTS calculate_orders;
+DROP FUNCTION IF EXISTS get_month_of_maximum_orders;
+DROP PROCEDURE IF EXISTS get_average_sale;
+DROP PROCEDURE IF EXISTS get_order_status_of_certain_period;
+
+#Create a function to calculate number of orders in a month.
+#Month and year will be input parameter to function.
 DELIMITER $$
 create function calculate_orders(month INT,year INT) RETURNS INT DETERMINISTIC
 BEGIN
@@ -12,30 +19,70 @@ BEGIN
  return result;
 END $$
 
+SELECT calculate_orders(04,2018);
+
+#Create a function to return month in a year having maximum orders.
+#Year will be input parameter.
 
 DELIMITER $
 create function get_month_of_maximum_orders(year INT) RETURNS INT DETERMINISTIC
 BEGIN
- DECLARE maximum, count, month INT;
- SET maximum=0;
- SET month=0;
- while(count<=12) DO
-  IF caculate_orders(count,year)>maximum then
-  SET maximum=calculate_orders(count,year);
-  SET month=count;
-  END IF;
-  SET count=count+1;
- END WHILE;
-
-return month;
+ DECLARE month INT;
+ 
+ SELECT max.max_month  into month FROM(
+ SELECT COUNT(date_of_order_placed) AS COUNT, MONTH(date_of_order_placed) AS max_month 
+ FROM orders where EXTRACT(YEAR from date_of_order_placed)=2018
+ GROUP BY MONTH(date_of_order_placed))max
+ ORDER BY max.count DESC LIMIT 1;
+ 
+ return month;
 END$
-
 
 SELECT get_month_of_maximum_orders(2018);
 
+#Create a Stored procedure to retrieve average sales of each product in a month.
+#Month and year will be input parameter to function.
 
+DELIMITER $@
+create procedure get_average_sale(year INT,month INT) 
 
+BEGIN
 
+SELECT  p.name,  sum(i.quantity_ordered) AS Quantity
+from item_list i,product p, orders o
+where i.order_number=o.id AND i.product_id=p.id AND month(o.date_of_order_placed)=month AND year(o.date_of_order_placed)=year
+GROUP BY i.product_id;
+
+END $@
+
+call get_average_sale(2018,04);
+
+#Create a stored procedure to retrieve table having order detail with status for a given period.
+#Start date and end date will be input parameter.
+# Put validation on input dates like start date is less than end date.
+# If start date is greater than end date take first date of month as start date.
+DELIMITER $&
+create procedure get_order_status_of_certain_period(start_date DATE, end_date DATE)
+
+BEGIN
+
+if start_date < end_date then
+
+SELECT p.name, i.quantity_ordered, i.status
+from orders o join item_list i join product p
+where o.id=i.order_number AND i.product_id=p.id AND o.date_of_order_placed between start_date AND end_date;
+
+else
+
+SELECT p.name, i.quantity_ordered, i.status
+from orders o join item_list i join product p
+where o.id=i.order_number AND i.product_id=p.id AND o.date_of_order_placed between DATE_FORMAT(end_date ,'%Y-%m-01') AND end_date;
+
+end if;
+
+end $&
+
+call get_order_status_of_certain_period("2018-01-10","2018-08-02");
 
 
 
