eb9c301e3c50d1afae435af4b2c5cc74e86638ec
meta-twinkle-garg <41181674+meta-twinkle-garg@users.noreply.github.com>
2018-09-05 13:03:49 +0530

    Update CategoryService.java

--- EAD-Session-5/RestfulWebService/src/com/service/CategoryService.java
+++ EAD-Session-5/RestfulWebService/src/com/service/CategoryService.java
@@ -7,14 +7,19 @@ import java.util.List;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import com.model.Advertisement;
 import com.model.Category;
+import com.dao.AdvertisementDAO;
 //import org.json.simple.JSONObject;
 import com.dao.CategoryDAO;
 
@@ -25,7 +30,10 @@ public class CategoryService {
 	@Path("/add")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.TEXT_PLAIN)
-	public Response addCategory(Category categoryObject) {
+	public Response addCategory(Category categoryObject,@HeaderParam("authorization") String authString) {
+		if(!("get-2018".equals(authString))){
+			return Response.status(Status.UNAUTHORIZED).entity("Incorrect Key").build();
+		}
 		CategoryDAO categoryDAOInstance = new CategoryDAO();
 		String categoryName = categoryObject.getName();
 		List<Category> existingCategories = getAllCategories();
@@ -63,5 +71,44 @@ public class CategoryService {
 		}
 		return listOfCategories;
 	}
+	
+	@GET
+	@Path("/{categoryId}/advertisements/")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Advertisement> getAdvertisementByCategory(@PathParam("categoryId") int categoryId){
+		AdvertisementDAO advertisementDAOInstance=new AdvertisementDAO();
+		List<Advertisement> listOfAdvertisements=new ArrayList<>();
+		ResultSet rset = advertisementDAOInstance.getAdvertisementByCategory(categoryId);
+		try {
+			while (rset.next()) {
+				Advertisement advertisement = new Advertisement();
+				advertisement.setId(rset.getInt("Id"));
+				advertisement.setTitle(rset.getString("Advertisement_Title"));
+				advertisement.setDescription(rset.getString("Advertisement_Desc"));
+				advertisement.setCategoryId(rset.getInt("Category_Id"));
+				listOfAdvertisements.add(advertisement);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return listOfAdvertisements;
+	}
+	@PUT
+	@Path("/update/{category}/{newCategory}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response updateAdvertisementByTitle(@PathParam("category") String category,@PathParam("newCategory") String newCategory,@HeaderParam("authorization") String authString){
+		if(!("get-2018".equals(authString))){
+			return Response.status(Status.UNAUTHORIZED).entity("Incorrect Key").build();
+		}
+		CategoryDAO CategoryDAOInstance = new CategoryDAO();
+		if(CategoryDAOInstance.updateCategory(category,newCategory)!=0){
+			return Response.ok("Category updated successfully..!",
+					MediaType.APPLICATION_JSON).build();
+		}else{
+			return Response.status(Status.NOT_MODIFIED).build();
+		}
+	}
 
+	
 }
